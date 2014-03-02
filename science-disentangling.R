e240463279f73a3fe27de770e11fc1e10e4de77d
Herb Susmann <herbps10@gmail.com>
2014-03-01 19:38:53 -0500

    Start of code to try to differentiate between biology and physics tests

---
+++ science-disentangling.R
@@ -0,0 +1,16 @@
+setwd("~/upstat-competition/")
+
+mcas = read.csv("~/Downloads/Student Data Competition/MCAS.csv")
+
+correct.answers = read.csv("correct-answers-physics-biology.csv", stringsAsFactors=FALSE)
+correct.answers$item.number = 1:45
+
+# Numerical scored questions don't help, so remove them
+correct.answers = correct.answers[correct.answers$Biology != 'Blank', ]
+
+multiple.choice.column.names = paste0("sitem", correct.answers$item.number)
+
+sum(mcas[1, multiple.choice.column.names] == correct.answers$Biology)
+
+print(sum(mcas[1, multiple.choice.column.names] == correct.answers$Biology))
+print(sum(mcas[1, multiple.choice.column.names] == correct.answers$Physics))
